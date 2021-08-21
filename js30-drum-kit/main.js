d621b49ba0bdf9cb5a0e9d9e8e88de278551449e
Anna <missjelxx@gmail.com>
2021-08-21 21:24:21 +0500

    feat: add application functions

--- js30-drum-kit/main.js
+++ js30-drum-kit/main.js
@@ -1,5 +1,9 @@
 const soundBlocks = document.querySelectorAll(".letter-wrapper");
-const container = document.querySelector(".container");
+const letters = document.querySelector(".letters");
+const container = document.querySelector('.container');
+const switcher = document.querySelector(".switch");
+
+let checked = false;
 
 const playSound = (block) => {
   let audio = document.getElementById(block.dataset.code);
@@ -8,15 +12,18 @@ const playSound = (block) => {
 
   let musicIcon = document.createElement("div");
   musicIcon.classList.add("music-icon");
-  block.append(musicIcon);
+  block.appendChild(musicIcon);
 
   audio.play();
   audio.currentTime = 0;
 
   block.addEventListener("transitionend", () => {
     block.classList.remove("active");
-    block.removeChild(musicIcon);
   });
+
+  setTimeout(() => {
+    block.removeChild(musicIcon);
+  }, 300);
 };
 
 const playSoundKeys = (e) => {
@@ -28,7 +35,7 @@ const playSoundKeys = (e) => {
 };
 
 const playSoundClick = (e) => {
-  if (!e.target.classList.contains("container")) {
+  if (!e.target.classList.contains("letters") ) {
     let targetCode = e.target.closest(".letter-wrapper").dataset.code;
     soundBlocks.forEach((block) => {
       if (targetCode === block.dataset.code) {
@@ -38,5 +45,25 @@ const playSoundClick = (e) => {
   }
 };
 
+const changeTheme = (checked) => {
+  if (!switcher.querySelector('input').checked) {
+    container.style.backgroundImage = "url('../img/background.jpg')"
+    soundBlocks.forEach(block => {
+      block.style.border = '3px solid #000';
+      block.style.background = 'rgba(0, 0, 0, 0.4)';
+    })
+  } else {
+    container.style.backgroundImage = "url('../img/background-dark.png')" 
+    soundBlocks.forEach(block => {
+      block.style.border = '2px solid #fff';
+      block.style.background = 'rgba(255, 255, 255, 0.05)';
+    })
+  }
+}
+
 document.addEventListener("keydown", playSoundKeys);
-container.addEventListener("click", playSoundClick);
+letters.addEventListener("click", playSoundClick);
+
+switcher.addEventListener('click', () => {
+  changeTheme(checked);
+});
